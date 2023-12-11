f903059f77daf51bf1a78157fbbcef9989f9c6c4
Muneeb ur rehman <bhatmuneeb0711@gmail.com>
2023-12-11 13:23:24 +0530

    created modal

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
 import TodoModal from './TodoModal';
 
 function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(true);
+
   return (
     <div className={styles.appHeader}>
-      <h1> hello from header</h1>
-      <Button variant="primary">Add Task</Button>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
       <SelectButton id="status">
         <option value="all">ALL</option>
         <option value="incomplete">InComplete</option>
         <option value="complete">Complete</option>
       </SelectButton>
-      <TodoModal />
+      <TodoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 }
--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,19 +1,53 @@
 import React from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
+import Button from './Button';
 
-function TodoModal() {
+function TodoModal({ modalOpen, setModalOpen }) {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.closeButton}>
-          <MdOutlineClose />
+    <div>
+      {modalOpen && (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div
+              className={styles.closeButton}
+              onClick={() => setModalOpen(false)}
+              onKeyDown={() => setModalOpen(false)}
+              tabIndex={0}
+              role="button"
+            >
+              <MdOutlineClose />
+            </div>
+            <form className={styles.form}>
+              <h1 className={styles.formTitle}> Add Task</h1>
+              <label htmlFor="title">
+                title
+                <input type="text" id="title" />
+              </label>
+              <label htmlFor="status">
+                Status
+                <select name="status" id=" status">
+                  <option value="incomplete">Incomplete</option>
+                  <option value="complete">Complete</option>
+                </select>
+              </label>
+              <div className={styles.buttoncontainer}>
+                <Button type="submit" variant="primary">
+                  Add Task
+                </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  onClick={() => setModalOpen(false)}
+                  onKeyDown={() => setModalOpen(false)}
+                >
+                  cancel
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
-        <form>
-          <h1 className={styles.formTitle}>Add Task</h1>
-        </form>
-        <h1> this is from modal </h1>
-      </div>
+      )}
     </div>
   );
 }
